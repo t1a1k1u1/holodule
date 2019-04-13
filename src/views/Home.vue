48ad67479cb4bf04d6be5d08444a362d3943c3a5
t1a1k1u1 <koishi.t81@gmail.com>
2019-04-13 20:43:30 +0900

    日付表示したりとか

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
     <div class="time-section" v-for="(line, index) in timeLine" :key="index">
-      <div class="datetime">{{ toDateTimeStr(line.time) }}</div>
-      <div class="live-list">
-        <div class="live" v-for="(live, index) in line.lives" :key="index" :style="bgColor(live.channels)">
-          <div class="time">{{ toTimeStr(live.start_at.seconds) }} ~</div>
-          <div class="channel-list">
-            <div class="channel" v-for="(channel, index) in live.channels" :key="index">
-              <img class="channel-image" :src="channelImage(channel)">
+      <div class="section-date" v-if="index === 0 || isStartOfDay(line.time)">{{ toDateStr(line.time.unix()) }}</div>
+      <div class="section-time">
+        <div class="time">{{ toTimeStr(line.time.unix()) }}</div>
+        <div class="live-list">
+          <div class="live" v-for="(live, index) in line.lives" :key="index" :style="bgColor(live.channels)">
+            <div class="live-time">{{ toTimeStr(live.start_at.seconds) }}</div>
+            <div class="channel-list">
+              <div class="channel" v-for="(channel, index) in live.channels" :key="index">
+                <img class="channel-image" :src="channelImage(channel)">
+              </div>
             </div>
           </div>
         </div>
@@ -63,8 +66,16 @@ export default class Home extends Vue {
     });
   }
 
+  private isStartOfDay(time: Moment.Moment): boolean {
+    return time.unix() === time.clone().startOf('day').unix();
+  }
+
   private toDateTimeStr(time: Moment.Moment): string {
     return time.format('MM/DD HH:mm');
+  }
+
+  private toDateStr(time: string): string {
+    return moment.unix(time).format('MM月DD日');
   }
 
   private toTimeStr(time: string): string {
@@ -88,50 +99,62 @@ export default class Home extends Vue {
 
 <style lang="scss" scoped>
 .time-section {
-  display: flex;
-  height: 64px;
 
-  .datetime {
+  .section-date {
     display: block;
-    width: 120px;
+    background-color: aliceblue;
+    margin: 8px 0;
   }
 
-  .live-list {
+  .section-time {
     display: flex;
-    align-items: center;
-    border-top: solid 2px gray;
-    width: 100%;
-    padding: 2px 0px;
+    height: 100px;
 
-    .live {
+    .time {
       display: block;
       position: relative;
-      background-color: aliceblue;
-      border-radius: 12px;
+      top: -0.5em;
+      margin: 0 4px;
+    }
+
+    .live-list {
+      display: flex;
+      align-items: center;
+      border-top: solid 2px gray;
       width: 100%;
-      height: 100%;
-      margin: 4px;
+      padding: 2px 0px;
 
-      .time {
-        position: absolute;
-        background-color: white;
-        border-radius: 60px;
-        top: 4px;
-        left: 4px;
-        padding: 2px 8px;
-      }
+      .live {
+        display: block;
+        position: relative;
+        background-color: aliceblue;
+        border-radius: 12px;
+        width: 100%;
+        height: 100%;
+        margin: 4px;
 
-      .channel-list {
-        display: flex;
-        justify-content: center;
-        height: inherit;
+        .live-time {
+          position: absolute;
+          background: rgba(white, 0.5);
+          border-radius: 8px;
+          top: 4px;
+          left: 4px;
+          padding: 2px 8px;
+          font-size: 8px;
+        }
 
-        .channel {
-          display: inline-block;
+        .channel-list {
+          display: flex;
+          justify-content: center;
           height: inherit;
 
-          .channel-image {
+          .channel {
+            display: inline-block;
             height: inherit;
+
+            .channel-image {
+              height: inherit;
+            }
           }
         }
       }
