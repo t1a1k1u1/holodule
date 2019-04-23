ea0492fa7093eadc4d8d44adc00e5152c2962400
t1a1k1u1 <koishi.t81@gmail.com>
2019-04-23 22:13:44 +0900

    remove function 'getSchedule'

--- functions/index.js
+++ functions/index.js
@@ -40,6 +40,7 @@ async function existTweet(tweet) {
 }
 
 function logTweet(tweet) {
+  console.log(tweet);
   db.collection('tweet').doc(tweet.id_str).set({
     tweet_id: Number(tweet.id_str),
     text    : tweet.text,
@@ -100,9 +101,7 @@ exports.setEvent = functions.region('asia-northeast1').https.onRequest(async (re
     console.log('not found holoduleTweet');
     return res.sendStatus(200);
   }
-  console.log(holoduleTweet);
   if (await existTweet(holoduleTweet)) {
-    console.log('already exist holoduleTweet');
     return res.sendStatus(200);
   }
 
@@ -131,111 +130,5 @@ exports.setEvent = functions.region('asia-northeast1').https.onRequest(async (re
       });
     }
   });
-  return res.sendStatus(200);
-});
-
-exports.getSchedule = functions.region('asia-northeast1').https.onRequest((req, res) => {
-  let client = new Twitter({
-    consumer_key       : `${functions.config().twitter.consumer.key}`,
-    consumer_secret    : `${functions.config().twitter.consumer.secret}`,
-    access_token_key   : `${functions.config().twitter.access_token.key}`,
-    access_token_secret: `${functions.config().twitter.access_token.secret}`,
-  });
-
-  const params = {
-    user_id    : '916156645004029952',
-    count      : 20,
-    include_rts: false,
-    tweet_mode : 'extended',
-  };
-
-  client.get('statuses/user_timeline', params, (error, tweets, response) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-
-    const holoduleTweet = _.find(tweets, (tweet) => {
-      return _.find(tweet.entities.hashtags, (hashtag) => {
-        return hashtag.text === 'ホロジュール';
-      });
-    });
-    if (!holoduleTweet) return;
-
-    const reply = _.find(tweets, (tweet) => {
-      return tweet.in_reply_to_status_id === holoduleTweet.id;
-    })
-
-    const holoduleTweetWithReply = _.concat(
-      holoduleTweet.full_text.split(/\r\n|\n/),
-      reply ? reply.full_text.split(/\r\n|\n/) : []
-    );
-
-    const reDay  = /本日.+(\d{1,2})\/(\d{1,2})/;
-    const reTime = /^(\d{1,2}):(\d{1,2})/;
-    const reName = /アキロゼ|さくらみこ|紫咲シオン|赤井はあと|湊あくあ|ときのそら|白上フブキ|ロボ子さん|百鬼あやめ|大空スバル|大神ミオ|夏色まつり|癒月ちょこ|夜空メル|猫又おかゆ|戌神ころね/g;
-
-    let currentTime;
-    let day;
-    let schedule = [];
-    _.forEach(holoduleTweetWithReply, (line) => {
-      const matched = {
-        day:   line.match(reDay),
-        time:  line.match(reTime),
-        names: line.match(reName),
-      };
-
-      if (matched.day) {
-        day = `${matched.day[1]}/${matched.day[2]}`;
-      }
-
-      if (day) {
-        if (matched.time) {
-          if (24 < Number(matched.time[1])) {
-            matched.time[1] = `${(Number(matched.time[1]) -24)}`;
-            currentTime = moment(`${day} ${matched.time[1]}:${matched.time[2]}`, 'MM/DD HH:mm').add(1, 'd');
-          } else {
-            currentTime = moment(`${day} ${matched.time[0]}`, 'MM/DD HH:mm');
-          }
-        }
-
-        if (matched.names) {
-          schedule.push({
-            time: currentTime,
-            names: matched.names,
-          });
-        }
-      }
-    });
-
-    if (schedule.length === 0) {
-      console.log('schedule not found');
-      return;
-    }
-
-    db.collection('schedules').where('start_at', '>=', schedule[0].time.toDate()).get().then((querySnapshot) => {
-      const holodule = [];
-      querySnapshot.forEach((doc) => { holodule.push(doc.data()) });
-
-      schedule.forEach((addLive) => {
-        if (
-          !_.find(holodule, (holoLive) => {
-            return (holoLive.start_at.seconds === addLive.time.unix())
-              && (_.difference(holoLive.channels, addLive.names).length === 0)
-          })
-        ) {
-          db.collection('schedules').add({
-            start_at: addLive.time.toDate(),
-            channels: addLive.names,
-          });
-        }
-      });
-      return;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  });
-
   return res.sendStatus(200);
 });
